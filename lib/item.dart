1e3b399ec5ba57853b6e6357712ad6e3a9c507cd
MatThHeuss <matheusalencar99@gmail.com>
2023-01-29 16:34:20 -0300

    add listagem de anuncions

--- lib/item.dart
+++ lib/item.dart
@@ -1,44 +1,76 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+List<Item> parseItems(String responseBody) {
+  final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
+  return parsed.map<Item>((json) => Item.fromJson(json)).toList();
+}
+
+Future<List<Item>> fetchItem() async {
+  final response =
+      await http.get(Uri.parse("http://10.0.2.2:8080/announcements"));
+
+  if (response.statusCode == 200) {
+    final test = parseItems(response.body);
+    test.map((e) => print(e.imageUrl.first)).toList();
+    return parseItems(response.body);
+  } else {
+    throw Exception('Failed to load item');
+  }
+}
+
 class Item {
   final String title;
   final String description;
-  final String imageUrl;
+  final List<String> imageUrl;
 
-  Item({
-    this.title = '',
-    this.description = '',
-    this.imageUrl = '',
+  const Item({
+    required this.title,
+    required this.description,
+    required this.imageUrl,
   });
+
+  factory Item.fromJson(Map<String, dynamic> json) {
+    return Item(
+      title: json['name'],
+      description: json['description'],
+      imageUrl: List<String>.from(json["images"].map((x) => x)),
+    );
+  }
 }
 
-final items = [
-  Item(
-    title: 'Mesa',
-    description: 'Mesa velha',
-    imageUrl:
-    'https://commerceplus.com.br/uploads/produto_fotos/2_20220224160945_462cf302732041a3966e0911ca9a6563.jpg',
-  ),
-  Item(
-    title: 'Cadeira',
-    description: 'Cadeira semi-nova',
-    imageUrl:
-    'https://abracasa.vteximg.com.br/arquivos/ids/175788-1000-1000/cadeira-lala-palha-preto-e-grafite-escuro-diagonal-traseira.jpg?v=637717330570930000',
-  ),
-  Item(
-    title: 'Cadeira 2',
-    description: 'Cadeira semi-nova',
-    imageUrl:
-    'https://abracasa.vteximg.com.br/arquivos/ids/175788-1000-1000/cadeira-lala-palha-preto-e-grafite-escuro-diagonal-traseira.jpg?v=637717330570930000',
-  ),
-  Item(
-    title: 'Notebook',
-    description: 'macbook pro m2',
-    imageUrl:
-    'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp-spacegray-select-202206?wid=640&hei=595&fmt=jpeg&qlt=95&.v=1664497359481',
-  ),
-  Item(
-    title: 'Macbook',
-    description: 'macbook pro m2',
-    imageUrl:
-    'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp-spacegray-select-202206?wid=640&hei=595&fmt=jpeg&qlt=95&.v=1664497359481',
-  ),
-];
+// final items = [
+//   Item(
+//     title: 'Mesa',
+//     description: 'Mesa velha',
+//     imageUrl:
+//         'https://commerceplus.com.br/uploads/produto_fotos/2_20220224160945_462cf302732041a3966e0911ca9a6563.jpg',
+//   ),
+//   Item(
+//     title: 'Cadeira',
+//     description: 'Cadeira semi-nova',
+//     imageUrl:
+//         'https://abracasa.vteximg.com.br/arquivos/ids/175788-1000-1000/cadeira-lala-palha-preto-e-grafite-escuro-diagonal-traseira.jpg?v=637717330570930000',
+//   ),
+//   Item(
+//     title: 'Cadeira 2',
+//     description: 'Cadeira semi-nova',
+//     imageUrl:
+//         'https://abracasa.vteximg.com.br/arquivos/ids/175788-1000-1000/cadeira-lala-palha-preto-e-grafite-escuro-diagonal-traseira.jpg?v=637717330570930000',
+//   ),
+//   Item(
+//     title: 'Notebook',
+//     description: 'macbook pro m2',
+//     imageUrl:
+//         'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp-spacegray-select-202206?wid=640&hei=595&fmt=jpeg&qlt=95&.v=1664497359481',
+//   ),
+//   Item(
+//     title: 'Macbook',
+//     description: 'macbook pro m2',
+//     imageUrl:
+//         'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp-spacegray-select-202206?wid=640&hei=595&fmt=jpeg&qlt=95&.v=1664497359481',
+//   ),
+// ];
--- lib/main.dart
+++ lib/main.dart
@@ -1,126 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'item.dart';
 import 'loading_screen.dart';
 import 'my_app.dart';
 
 void main() {
-  runApp(MaterialApp(
-    theme: ThemeData(
-        primarySwatch: Colors.indigo,
-        textTheme: const TextTheme(
-          bodyText1: TextStyle(color: Colors.white),
-          headline6: TextStyle(color: Colors.white),
-          subtitle2: TextStyle(color: Colors.white),
-        )),
-    home: const LoadingScreen(),
-  ));
+  runApp(const LoadingScreen());
 }
 
-class LoadingScreen extends StatefulWidget {
+class LoadingScreen extends StatelessWidget {
   const LoadingScreen({super.key});
-
-  @override
-  LoadingScreenState createState() => LoadingScreenState();
-
-}
-
-class LoadingScreenState extends State<LoadingScreen> {
-  Future<Widget> _loadApp() async {
-    // Perform actual app loading here
-    // For example, fetching data from a server
-    //var data = await fetchDataFromServer();
-    await Future.delayed(const Duration(seconds: 2));
-    return const MyApp(); //Here
-  }
-
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<Widget>(
-      future: _loadApp(),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return snapshot.data!;
-        } else if (snapshot.hasError) {
-          return Text("Error: ${snapshot.error}");
-        } else {
-          return Scaffold(
-            body: Container(
-              color: Colors.indigo,
-              child: const Center(
-                child: Text(
-                  'iDonate',
-                  style: TextStyle(color: Colors.white, fontSize: 60),
-                ),
-              ),
-            ),
-          );
-        }
-      },
-    );
-  }
-}
-
-class MyApp extends StatelessWidget {
-
-  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-          title: const Text('iDonate'),
-          backgroundColor: Theme.of(context).colorScheme.primary),
-      body: ListView.builder(
-        itemCount: items.length,
-        itemBuilder: (context, index) {
-          return Card(
-            child: Column(
-              children: <Widget>[
-                Image.network(
-                  items[index].imageUrl,
-                  fit: BoxFit.cover,
-                  height: 150,
-                ),
-                ListTile(
-                  title: Text(items[index].title),
-                  subtitle: Text(items[index].description),
-                ),
-              ],
-            ),
-          );
-        },
-      ),
-      drawer: Theme(
-        data: Theme.of(context).copyWith(
-          canvasColor: Theme.of(context).colorScheme.secondary,
+      title: 'IDonate',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const LoadingScreenPage(),
+    );
+  }
+}
+
+class LoadingScreenPage extends StatelessWidget {
+  const LoadingScreenPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter ListView'),
         ),
-        child: Drawer(
-          child: ListView(
-            children: <Widget>[
-              const DrawerHeader(
-                decoration: BoxDecoration(
-                    //color: Color(0xFF0E1647),
-                    ),
-                child: Text('iDonate'),
-              ),
-              ListTile(
-                title: const Text('Configurações'),
-                onTap: () {
-                  // Handle tap
-                },
-              ),
-              ListTile(
-                title: const Text('Sair'),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-            ],
-          ),
-        ),
-      ),
-    ));
+        body: const MyStatefulWidget());
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({super.key});
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<Item>>(
+      future: fetchItem(),
+      builder: (context, snapshot) {
+        print(snapshot.data![0].imageUrl[0]);
+        if (snapshot.hasData) {
+          return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Card(
+                  child: Column(
+                    children: <Widget>[
+                      Image.network(
+                        snapshot.data![index].imageUrl[1],
+                        fit: BoxFit.cover,
+                        height: 150,
+                      ),
+                      ListTile(
+                        title: Text(snapshot.data![index].title),
+                        subtitle: Text(snapshot.data![index].description),
+                      ),
+                    ],
+                  ),
+                );
+              });
+        } else if (snapshot.hasError) {
+          return Text(snapshot.error.toString());
+        }
+        // By default show a loading spinner.
+        return const CircularProgressIndicator();
+      },
+    );
   }
 }
