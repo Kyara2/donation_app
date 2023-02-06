676983047ee0dbb72e723f1d3245599ea4b6afff
MatThHeuss <matheusalencar99@gmail.com>
2023-02-06 18:12:36 -0300

    get account details

---
+++ lib/get_account.dart
@@ -0,0 +1,52 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+AccountDetails parseAccountDetails(String responseBody) {
+  final responseBodyUTF8 = utf8.decode(responseBody.runes.toList());
+  final parsed = json.decode(responseBodyUTF8).cast<Map<dynamic, dynamic>>();
+  return parsed
+      .map<AccountDetails>((json) => AccountDetails.fromJson(json))
+      .toList();
+}
+
+Map<String, String> queryParameters = {"email": "teste2@email.com"};
+
+final newURI = Uri.parse("http://10.0.2.2:8080/users").replace(queryParameters: queryParameters);
+
+Future<AccountDetails> fetchAccountDetails() async {
+  print(newURI);
+  final response = await http.get(newURI); //mudar
+  if (response.statusCode == 200) {
+    print(response.body);
+    return AccountDetails.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to load account details');
+  }
+}
+
+class AccountDetails {
+  final String id;
+  final String name;
+  final String email;
+  final String profileImage;
+  final String createdAt;
+
+  const AccountDetails({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.profileImage,
+    required this.createdAt
+  });
+
+  factory AccountDetails.fromJson(Map<String, dynamic> json) {
+    return AccountDetails(
+        id: json['id'],
+        name: json['name'],
+        email: json['email'],
+        profileImage: json['profile_image'],
+        createdAt: json['created_at']);
+  }
+}
